5067c938d368a2cca52084d34f455d47e6ace7a5
bryce_000 <bs482@pvsd.org>
2018-09-10 15:51:45 -0400

    imported code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,18 @@
 package com.company;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner kb = new Scanner(System.in);
+        System.out.println("What is your first name?");
+        String first = kb.nextLine();
+        System.out.println("What is your last name?");
+        String last = kb.nextLine();
+        System.out.println("Your full name is " + first + " " + last);
+
+
+
     }
 }
